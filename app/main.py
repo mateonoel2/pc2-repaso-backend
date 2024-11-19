41802b42d771ee80f1f0650ab30e202e577bf082
mateonoel2 <mateo.noel@utec.edu.pe>
2024-11-19 06:50:05 -0500

    Add CORS middleware to FastAPI application

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from api import auth, anime, favorites, history
 from models.database import Base, engine
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 
